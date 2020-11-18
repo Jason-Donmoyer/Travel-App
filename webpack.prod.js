598de4d6ccf8207789dba525e1f134e616ce7271
Jason-Donmoyer <j.donmoyer523@gmail.com>
2020-11-18 17:59:12 -0500

    set up webpack and dependencies

---
+++ webpack.prod.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const {
+  CleanWebpackPlugin
+} = require("clean-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = {
+  entry: './src/client/index.js',
+  mode: 'production',
+  output: {
+    filename: "[name].[contentHash].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: 'var',
+    library: 'Client',
+  },
+  optimization: {
+    minimizer: [new OptimizeCssAssetsWebpackPlugin({}), new TerserPlugin({})],
+  },
+  module: {
+    rules: [{
+        test: '/.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.png$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contentHash].css"
+    }),
+    new CleanWebpackPlugin({
+      dry: false,
+      verbose: true,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+    new WorkboxPlugin.GenerateSW(),
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
